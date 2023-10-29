cb02120fd0dfc31aff9883bab4c0f8b4bd117504
idkagooduser <98674911+idkagooduser@users.noreply.github.com>
2023-10-29 11:33:51 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+var noseX=0;
+var noseY=0;
+var leftWristX=0;
+var rightWristX=0;
+var diff=0;
+
+function setup(){
+    canvas=createCanvas(500,500);
+    canvas.position(600,100);
+
+    video=createCapture(VIDEO);
+    video.size(500,600);
+    video.position(20,50);
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+
+};
+
+function modelLoaded(){
+    console.log("model is ready");
+}
+
+function gotPoses(results){
+if(results.length>0){
+    console.log(results);
+
+    noseX=results[0].pose.nose.x;
+    noseY=results[0].pose.nose.y;
+    leftWristX=results[0].pose.leftWrist.x;
+    rightWristX=results[0].pose.rightWrist.x;
+    diff=floor(leftWristX-rightWristX);
+}
+}
+
+
+function draw(){
+    document.getElementById("size").innerHTML="Width and height of the square will be: "+diff +" pixels";
+    background("teal");
+stroke(76,80,90);
+fill(76,80,90);
+rectMode(CENTER);    
+square(noseX,noseY,diff);
+} 
